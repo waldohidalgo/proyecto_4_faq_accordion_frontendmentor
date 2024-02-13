bee6d115759158a86cd2aac95fefb89211b25ea3
Waldo Hidalgo <whidalgohp@gmail.com>
2024-02-12 21:42:56 -0300

    cargo proyecto listo 🎉

---
+++ README.md
@@ -0,0 +1,33 @@
+# Frontend Mentor - FAQ accordion solution
+
+Esta es mi solución al desafío [FAQ accordion challenge on Frontend Mentor](https://www.frontendmentor.io/challenges/faq-accordion-wyfFdeBwBz). He utilizado las siguientes tecnologías:
+
+![HTML5](https://img.shields.io/badge/html5-%23E34F26.svg?style=for-the-badge&logo=html5&logoColor=white)![CSS3](https://img.shields.io/badge/css3-%231572B6.svg?style=for-the-badge&logo=css3&logoColor=white)![JavaScript](https://img.shields.io/badge/javascript-%23323330.svg?style=for-the-badge&logo=javascript&logoColor=%23F7DF1E)
+
+## Overview
+
+### The challenge
+
+Users should be able to:
+
+- Hide/Show the answer to a question when the question is clicked. **Realizado**
+
+- Navigate the questions and hide/show answers using keyboard navigation alone: **Realizado**: he utilizado el evento onkeydown con las teclas **ArrowDown**,**ArrowUp** para navegar entre las filas del accordion y **Enter** para mostrar/ocultar el contenido de la fila del accordion.
+
+- View the optimal layout for the interface depending on their device's screen size: Proyecto responsivo en base a las medidas de las imagenes de diseño móvil y de escritorio.
+
+- See hover and focus states for all interactive elements on the page:**Realizado**: Cambia color al hacer hover sobre el encabezado de cada fila del accordion o al desplazarse sobre cada fila usando el teclado el cual desaparece al hacer click sobre la fila o al presionar Enter sobre la fila.
+
+### Screenshots
+
+#### 1. Pantallas Grandes
+
+![Pantallas Grandes](./assets/images/screenshots/big_screens-min.png)
+
+#### 2. Pantallas Small
+
+![Pantallas Grandes](./assets/images/screenshots/small_screens-min.png)
+
+### Links
+
+- Live Site URL: [Add live site URL here](https://your-live-site-url.com)
---
+++ assets/js/script.js
@@ -0,0 +1,113 @@
+const card_row_acordion_headers = document.querySelectorAll(
+  ".card_row_acordion_header"
+);
+/*
+Activar Nodos con clase activo
+*/
+function activateClassActive() {
+  const wrapper_acordion_row_contenido_activos = document.querySelectorAll(
+    ".wrapper_acordion_row_contenido.activo"
+  );
+  for (let nodoActivo of wrapper_acordion_row_contenido_activos) {
+    nodoActivo.style.height = `${
+      nodoActivo.querySelector(".card_row_acordion_contenido").clientHeight
+    }px`;
+  }
+}
+document.addEventListener("DOMContentLoaded", activateClassActive);
+
+/*
+-----------------------
+*/
+function removeClaseActivo(i) {
+  for (let j = 0; j < card_row_acordion_headers.length; j++) {
+    const nodoActivo = card_row_acordion_headers[j];
+    const nodoPadre = nodoActivo.parentElement;
+    const contenido_acordion = nodoPadre.querySelector(
+      ".wrapper_acordion_row_contenido"
+    );
+    const image = nodoPadre.querySelector(
+      ".card_row_acordion_contenedor_img img"
+    );
+    if (j != i && contenido_acordion.classList.contains("activo")) {
+      image.src = "./assets/images/icon-plus.svg";
+      contenido_acordion.classList.remove("activo");
+      contenido_acordion.style.height = "0px";
+      nodoActivo.classList.remove("card_row_acordion_header--activo");
+    }
+  }
+}
+
+function toggleClasesAndImages(card_row_acordion_header) {
+  const contenido_acordion =
+    card_row_acordion_header.parentElement.querySelector(
+      ".wrapper_acordion_row_contenido"
+    );
+  contenido_acordion.classList.toggle("activo");
+
+  const image = card_row_acordion_header.querySelector(
+    ".card_row_acordion_contenedor_img img"
+  );
+  if (contenido_acordion.classList.contains("activo")) {
+    image.src = "./assets/images/icon-minus.svg";
+    contenido_acordion.style.height = `${
+      card_row_acordion_header.parentElement.querySelector(
+        ".card_row_acordion_contenido"
+      ).clientHeight
+    }px`;
+  } else {
+    image.src = "./assets/images/icon-plus.svg";
+    contenido_acordion.style.height = "0px";
+  }
+}
+
+for (let i = 0; i < card_row_acordion_headers.length; i++) {
+  const card_row_acordion_header = card_row_acordion_headers[i];
+  card_row_acordion_header.addEventListener("click", function () {
+    removeClaseActivo(i);
+    toggleClasesAndImages(card_row_acordion_header);
+  });
+}
+
+/*
+Control Acordion Teclado
+*/
+
+let indexAcordionElemento = -1;
+document.onkeydown = function (evento) {
+  if (evento.key == "ArrowDown") {
+    removeHover();
+    indexAcordionElemento =
+      (indexAcordionElemento + 1) % card_row_acordion_headers.length;
+
+    card_row_acordion_headers[indexAcordionElemento].classList.toggle(
+      "card_row_acordion_header--activo"
+    );
+  }
+  if (evento.key == "ArrowUp") {
+    if (indexAcordionElemento == -1) {
+      indexAcordionElemento = card_row_acordion_headers.length - 1;
+    } else {
+      indexAcordionElemento =
+        indexAcordionElemento - 1 < 0
+          ? card_row_acordion_headers.length - 1
+          : indexAcordionElemento - 1;
+    }
+
+    removeHover();
+    card_row_acordion_headers[indexAcordionElemento].classList.toggle(
+      "card_row_acordion_header--activo"
+    );
+  }
+
+  if (evento.key == "Enter" && indexAcordionElemento != -1) {
+    removeHover();
+    card_row_acordion_headers[indexAcordionElemento].click();
+  }
+};
+
+function removeHover() {
+  for (let nodo of card_row_acordion_headers) {
+    nodo.classList.remove("card_row_acordion_header--activo");
+  }
+}
